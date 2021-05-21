be4e62ca3c08e3c24734690d35764a6457eb4cef
eklz <epfklotz@gmail.com>
2021-05-21 18:04:50 +0200

    Trial on real data
    Signed-off-by: eklz <epfklotz@gmail.com>

--- Codes/Eigen_values.py
+++ Codes/Eigen_values.py
@@ -4,8 +4,8 @@ from Legendre import *
 
 
 def roots_leg(n, t):
-    Pk0 = np.poly1d([1])
-    Pk1 = np.poly1d([1, -1/len(t)*np.sum(t)])
+    Pk0 = np.poly1d([1]) # 1
+    Pk1 = np.poly1d([1, -1/len(t)*np.sum(t)]) # x - (1/n)*sum(t)
     Bk = np.sum([i*Pk1(i)**2 for i in t])
     Bk = Bk/np.sum([Pk1(i)**2 for i in t])
     J = np.zeros((n, n))
@@ -19,7 +19,7 @@ def roots_leg(n, t):
         Gk = Gk/np.sum([Pk0(i)**2 for i in t])
 
         beta = np.sqrt(Gk)
-        Pk2 = Pk1*np.poly1d([1, -Bk]) - Gk*Pk0
+        Pk2 = Pk1*np.poly1d([1, -Bk]) - Gk*Pk0 
 
         Pk0, Pk1 = Pk1, Pk2
 
@@ -27,10 +27,12 @@ def roots_leg(n, t):
         J[j+1][j] = beta
         J[j][j+1] = beta
     J[n-1][n-1] = Bk
-    return J
+    return np.linalg.eig(J)[0]
+
+
 
 
 # %%
 J = roots_leg(10, [i/1000 for i in range(1000)])
-np.linalg.eig(J)[0]
+J
 # %%
--- Codes/Legendre.py
+++ Codes/Legendre.py
@@ -22,6 +22,9 @@ def Legendre(n, t):
         Pk0, Pk1 = Pk1, Pk2
 
     return Pk2
+
+
+
 # %%
 
 
@@ -58,6 +61,55 @@ def Laguerre(m, Pm, sigma=0.001):
 
     return np.sort(res)
 
+def roots_leg(n, t):
+    Pk0 = np.poly1d([1]) # 1
+    Pk1 = np.poly1d([1, -1/len(t)*np.sum(t)]) # x - (1/n)*sum(t)
+    Bk = np.sum([i*Pk1(i)**2 for i in t])
+    Bk = Bk/np.sum([Pk1(i)**2 for i in t])
+    J = np.zeros((n, n))
+    for j in range(0, n-1):
+
+        alpha = Bk
+
+        Bk = np.sum([i*Pk1(i)**2 for i in t])
+        Bk = Bk/np.sum([Pk1(i)**2 for i in t])
+        Gk = np.sum([i*Pk1(i)*Pk0(i) for i in t])
+        Gk = Gk/np.sum([Pk0(i)**2 for i in t])
+
+        beta = np.sqrt(Gk)
+        Pk2 = Pk1*np.poly1d([1, -Bk]) - Gk*Pk0 
+
+        Pk0, Pk1 = Pk1, Pk2
+
+        J[j][j] = alpha
+        J[j+1][j] = beta
+        J[j][j+1] = beta
+    J[n-1][n-1] = Bk
+    return np.linalg.eig(J)[0]
+
+
+def Lagrange(i, roots):
+    L = np.poly1d([1]) # 1
+    ai = roots[i]
+    for a in np.delete(roots, i):
+        L = L*1/(ai-a)*np.poly1d([1, -a])
+    return L
+
+
+def interp_gauss_legendre(n, t, y):
+    res = []
+    roots = roots_leg(n, t)
+    roots = np.sort(roots)
+    if len(t) != len(y):
+        raise ValueError(f't and y must have the same length, t has length {len(t)} and y {len(y)}')
+    
+    for i in range(n):
+        L = Lagrange(i, roots)
+        norm =np.sum([L(j)**2 for j in t])
+        sum = np.sum([L(t[k])*y[k] for k in range(len(y))])
+        res.append(1/norm * sum)
+        
+    return roots, res
 
 # %%
 if __name__ == "__main__":
